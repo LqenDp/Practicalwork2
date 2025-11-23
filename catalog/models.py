f2120291617e34420443d479d9053e6a08d1705d
LqenDp <oorzaksancaj7@gmail.com>
2025-11-24 04:38:12 +0700

    models

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=200, help_text="Введите категорию дизайна")
 
-# Create your models here.
+    def __str__(self):
+        return self.name
